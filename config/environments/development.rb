ce1436eddccd5ccd9be9c51b1fb12e579cc29c70
Yves-Medhard Tibe da Cunha Tibe-Bi <yves.medhard@codus.com.br>
2019-04-09 20:54:18 -0300

    data

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.action_mailer.default_url_options = { protocol: 'http', host: 'localhost', port: '1025' }
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
